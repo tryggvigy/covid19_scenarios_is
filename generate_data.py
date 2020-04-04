11ad8bce556cb107e38a65801d48cbcdda199300
Tryggvi Gylfason <tryggvi@spotify.com>
2020-04-04 19:37:16 +0000

    Schema validation

--- generate_data.py
+++ generate_data.py
@@ -3,24 +3,104 @@ import requests
 import io
 from pathlib import Path
 import pandas as pd
+from pandas_schema import Column, Schema
+from pandas_schema.validation import \
+    DateFormatValidation, \
+    LeadingWhitespaceValidation, \
+    TrailingWhitespaceValidation, \
+    CustomSeriesValidation, \
+    CanConvertValidation
 
 URL_CASES = "https://raw.githubusercontent.com/tryggvigy/CoronaWatchIS/master/data/covid_in_is.cvs"
 URL_HOSPITALIZED = "https://raw.githubusercontent.com/tryggvigy/CoronaWatchIS/master/data/covid_in_is_hosp.cvs"
 
+date_validator = Column('date', [
+    LeadingWhitespaceValidation(),
+    TrailingWhitespaceValidation(),
+    DateFormatValidation("%Y-%m-%d") & CustomSeriesValidation(
+        lambda x: x.is_monotonic_increasing and x.is_unique,
+        'date is not monotonic')
+])
+
+default_value_validators = [
+    LeadingWhitespaceValidation(),
+    TrailingWhitespaceValidation()
+]
+
+schemas_by_key = {
+    'cases':
+    Schema([
+        date_validator,
+        Column('cases', [
+            *default_value_validators,
+            CanConvertValidation(int) & CustomSeriesValidation(
+                lambda x: x.is_monotonic_increasing, 'cases is not monotonic')
+        ])
+    ]),
+    'deaths':
+    Schema([
+        date_validator,
+        Column('deaths', [
+            *default_value_validators,
+            CanConvertValidation(int) & CustomSeriesValidation(
+                lambda x: x.is_monotonic_increasing, 'deaths is not monotonic')
+        ])
+    ]),
+    'recovered':
+    Schema([
+        date_validator,
+        Column('recovered', [
+            *default_value_validators,
+            CanConvertValidation(int)
+            & CustomSeriesValidation(lambda x: x.is_monotonic_increasing,
+                                     'recovered is not monotonic')
+        ])
+    ]),
+    'hospitalized':
+    Schema([
+        date_validator,
+        Column('hospitalized',
+               [*default_value_validators,
+                CanConvertValidation(int)])
+    ]),
+    'ICU':
+    Schema([
+        date_validator,
+        Column('ICU', [*default_value_validators,
+                       CanConvertValidation(int)])
+    ])
+}
+
+
+def validate(df):
+    key = df.columns[1]
+    violations = schemas_by_key[key].validate(df)
+
+    for violation in violations:
+        print(violation, file=sys.stderr)
+
+    if len(violations):
+        print(f"Validation on {key} failed", file=sys.stderr)
+        raise Exception(violations)
+
+
 def parse(csv_text, input_data_col, data_col, cumsum=False):
     fd = io.StringIO(csv_text)
-    df = pd.read_csv(fd, usecols = ['Dagsetning' , input_data_col], header=0)
+    df = pd.read_csv(fd, usecols=['Dagsetning', input_data_col], header=0)
 
     # set column header names
     df.columns = ['date', data_col]
 
     # set date format YYYY-mm-dd
-    df['date'] = pd.to_datetime(df.date)
+    df['date'] = pd.to_datetime(df.date).dt.date
 
     # Apply cumulative sum to data
     if cumsum: df[data_col] = df[data_col].cumsum()
 
+    validate(df)
+
     return df
+
 
 def parse_csv(url, input_data_col, data_col, filename, cumsum=False):
     r = requests.get(url)
@@ -31,8 +111,6 @@ def parse_csv(url, input_data_col, data_col, filename, cumsum=False):
 
     df = parse(r.text, input_data_col, data_col, cumsum)
 
-    print(df)
-
     df.to_csv(str(Path('data', filename)), index=False)
 
 
@@ -42,15 +120,19 @@ def build_data():
 
     print('Downloading')
     parse_csv(URL_CASES, 'Smit_Samtals', 'cases', 'cumulative_cases.cvs')
-    parse_csv(URL_CASES, 'Dauðsföll', 'deaths', 'cumulative_deaths.cvs', cumsum=True)
-    parse_csv(URL_CASES, 'Batnað_Samtals', 'recovered', 'cumulative_recovered.cvs')
-    parse_csv(URL_HOSPITALIZED, 'Spitali_Samtals', 'hospitalized', 'cumulative_hospitalized.cvs')
+    parse_csv(URL_CASES, 'Dauðsföll_Samtals', 'deaths',
+              'cumulative_deaths.cvs')
+    parse_csv(URL_CASES, 'Batnað_Samtals', 'recovered',
+              'cumulative_recovered.cvs')
+    # should not be cumulative counts
+    parse_csv(URL_HOSPITALIZED, 'Inniliggjandi', 'hospitalized',
+              'cumulative_hospitalized.cvs')
     # Looks like this column should not be cumulative sum because Gjosgaesal_Samtals
     # also exists but doesn't make sense (has no data until 2020-03-27, then spikes to 11).
     # This is the cumulative sum column with the correct data.
     parse_csv(URL_HOSPITALIZED, 'Gjorgaesla', 'ICU', 'cumulative_icu.cvs')
-
     print('Success')
+
 
 if __name__ == '__main__':
     build_data()
--- test_generate_data.py
+++ test_generate_data.py
@@ -2,45 +2,37 @@ import unittest
 import pandas as pd
 from generate_data import parse
 
+
 def assert_csv_equal(test, df, expected):
     test.assertEqual(df.to_csv(index=False).rstrip(), expected)
 
-class TestGenerateData(unittest.TestCase):
 
+class TestGenerateData(unittest.TestCase):
     def test_transform_header(self):
-        df = parse('Dagsetning,Smit_Samtals', 'Smit_Samtals', 'cases')
-        assert_csv_equal(self, df, 'date,cases')
+        df = parse(('Dagsetning,Smit_Samtals\n'
+                    '2020-03-29,156'), 'Smit_Samtals', 'cases')
+        assert_csv_equal(self, df, ('date,cases\n' '2020-03-29,156'))
 
     def test_corrects_datetime_format(self):
-        df = parse(
-            ('Dagsetning,Smit_Samtals\n'
-             '03-29-2020,156\n'
-             '03-30-2020,186'),
-            'Smit_Samtals',
-            'cases'
-        )
-        assert_csv_equal(
-            self,
-            df,
-            ('date,cases\n'
-             '2020-03-29,156\n'
-             '2020-03-30,186')
-        )
+        df = parse(('Dagsetning,Smit_Samtals\n'
+                    '03-29-2020,156\n'
+                    '03-30-2020,186'), 'Smit_Samtals', 'cases')
+        assert_csv_equal(self, df, ('date,cases\n'
+                                    '2020-03-29,156\n'
+                                    '2020-03-30,186'))
 
     def test_cumulative_sum(self):
-        df = parse(
-            ('Dagsetning,Smit_Samtals\n'
-             '2020-03-29,0\n'
-             '2020-03-30,1\n'
-             '2020-03-31,0\n'
-             '2020-04-01,2\n'
-             '2020-04-02,0\n'
-             '2020-04-03,0\n'
-             '2020-04-04,5'),
-            'Smit_Samtals',
-            'cases',
-            cumsum=True
-        )
+        df = parse(('Dagsetning,Smit_Samtals\n'
+                    '2020-03-29,0\n'
+                    '2020-03-30,1\n'
+                    '2020-03-31,0\n'
+                    '2020-04-01,2\n'
+                    '2020-04-02,0\n'
+                    '2020-04-03,0\n'
+                    '2020-04-04,5'),
+                   'Smit_Samtals',
+                   'cases',
+                   cumsum=True)
         assert_csv_equal(
             self,
             df,
@@ -53,6 +45,34 @@ class TestGenerateData(unittest.TestCase):
              '2020-04-03,3\n'
              '2020-04-04,8'),
         )
+
+    def test_validation_date_format(self):
+        with self.assertRaises(Exception) as cm:
+            parse(('Dagsetning,Smit_Samtals\n'
+                   ',156'), 'Smit_Samtals', 'cases')
+        exceptions = cm.exception.args[0]
+        exception = exceptions[0]
+        self.assertRegex(str(exceptions[0]),
+                         'does not match the date format string "%Y-%m-%d"')
+
+    def test_validation_date_monotonic(self):
+        with self.assertRaises(Exception) as cm:
+            parse(('Dagsetning,Smit_Samtals\n'
+                   '2020-03-29,156\n'
+                   '2020-03-28,156'), 'Smit_Samtals', 'cases')
+        exceptions = cm.exception.args[0]
+        exception = exceptions[0]
+        self.assertRegex(str(exceptions[0]), 'date is not monotonic')
+
+    def test_validation_cases_monotonic(self):
+        with self.assertRaises(Exception) as cm:
+            parse(('Dagsetning,Smit_Samtals\n'
+                   '2020-03-29,156\n'
+                   '2020-03-30,16'), 'Smit_Samtals', 'cases')
+        exceptions = cm.exception.args[0]
+        exception = exceptions[0]
+        self.assertRegex(str(exceptions[0]), 'cases is not monotonic')
+
 
 if __name__ == '__main__':
     unittest.main()
